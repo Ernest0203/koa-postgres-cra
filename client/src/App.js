733e74fc061e276ba8e7da043cc13d05b19c4914
Ernest0203 <ernest2332@gmail.com>
2020-02-21 16:19:57 +0200

    add some structure, store, saga

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import styled, { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <GlobalStyle />
+      <Provider store={store}>
+        <AppWrapper>
+          <header className="App-header">
+            Koa-postgres-cra
+          </header>
+        </AppWrapper>
+      </Provider>
+    </Fragment>
   );
 }
+
+const AppWrapper = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
 
 export default App;
