7c949b7c76e32d7c179cd0c70b094cab74965330
Ernest0203 <ernest2332@gmail.com>
2020-02-28 19:54:29 +0200

    basic styles

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,10 @@ function App() {
       <GlobalStyle />
       <Provider store={store}>
         <AppWrapper>
-          <header className="App-header">
-            Koa-postgres-cra
+          <header className="appHeader">
+            <h2>Koa-postgres-cra</h2>
           </header>
+          <main className="appMain"></main>
         </AppWrapper>
       </Provider>
     </Fragment>
@@ -21,10 +22,19 @@ function App() {
 const AppWrapper = styled.div`
   width: 1200px;
   margin: 0 auto;
+  box-shadow: 0 3px 10px rgba(0,0,0, .2);
+  background-color: #fff;
+  .appHeader {
+    padding: 20px 0;
+    h2 {
+      text-align: center;
+    }
+  }
 `;
 
 const GlobalStyle = createGlobalStyle`
   body {
+    background-color: #eee;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
